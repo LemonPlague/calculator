decb6dc9b2db5feae6f11e72f79a21a9b00cff42
Juice <moorerichardplease@gmail.com>
2023-11-27 17:47:57 +0900

    added basic math functions

---
+++ script.js
@@ -0,0 +1,23 @@
+//-------------VARIABLES-------------
+
+let runningTotal = 0;
+let secondNumber = 0;
+let operator = '';
+
+//-------------FUNCTIONS-------------
+
+function Addition(a, b) {
+    return a + b;
+}
+
+function Subtraction(a, b) {
+    return a - b;
+}
+
+function Multiplication(a, b) {
+    return a * b;
+}
+
+function Division(a, b) {
+    return a / b;
+}
