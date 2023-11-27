2d184f49db485183d99f6b6b5b22ce8ae3ab6065
Juice <moorerichardplease@gmail.com>
2023-11-27 17:51:36 +0900

    added operations function

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ function Multiplication(a, b) {
 function Division(a, b) {
     return a / b;
 }
+
+function operations(a, b, sign) {
+    const operators = ['+', '-', '*', '/'];
+    operators.forEach((operator) => {
+        if (sign == '+') {
+            Addition(a, b);
+        } else if (sign == '-') {
+            Subtraction(a, b);
+        } else if (sign == '*') {
+            Multiplication(a, b);
+        } else if (sign == '/') {
+            Division(a, b);
+        }
+    });
+}
